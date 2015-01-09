ef54d1d347eb402fd945a9957c791104ccfe5027
xcrunner0921 <xcrunner0921@gmail.com>
2015-01-09 15:56:51 +0900

    : API
    Task-Url:

--- api/src/main/java/api/subway/dao/ApiDao.java
+++
@@ -1,9 +0,0 @@
-package api.subway.dao;
-
-import org.apache.ibatis.session.SqlSession;
-
-public interface ApiDao {
-	public void setSqlSession(SqlSession sqlSession);
-
-	//public User getUser(String userId);
-}
